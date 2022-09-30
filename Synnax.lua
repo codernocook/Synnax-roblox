c3d62fd028b13c23483261a3ada96c271e971cee
codernocook <90613458+codernocook@users.noreply.github.com>
2022-09-30 16:41:02 +0700

    Update Synnax.lua

--- Synnax.lua
+++ Synnax.lua
@@ -916,12 +916,15 @@ local Synnax = {
                                                 task.wait(game:GetService("Players").RespawnTime - 0.1)
                                                 oldrespawnpos = speaker.Character:GetPivot()
                                                 oldcameracframe = game:GetService("Workspace").CurrentCamera.CFrame
-                                                task.wait(0.2)
+                                                task.wait(0.1)
+                                                speaker.Character:FindFirstChildWhichIsA("Humanoid"):SetStateEnabled(Enum.HumanoidStateType.Jumping, false)
+                                                task.wait(0.1)
                                                 repeat task.wait() until speaker.Character and game:GetService("Workspace").CurrentCamera
                                                 speaker.Character:PivotTo(oldrespawnpos)
                                                 game:GetService("Workspace").CurrentCamera.CFrame = oldcameracframe
                                                 speaker.Character:PivotTo(oldrespawnpos)
                                                 game:GetService("Workspace").CurrentCamera.CFrame = oldcameracframe
+                                                speaker.Character:FindFirstChildWhichIsA("Humanoid"):SetStateEnabled(Enum.HumanoidStateType.Jumping, true)
                                                 BetterGodModeRespawnCheck = false
                                             end
                                         end
